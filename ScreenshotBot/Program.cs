39c0b618376fd788d55ccd0a3ec91a8e38c311df
Fed511 <federicouni350@gmail.com>
2025-07-16 14:45:02 +0200

    Aggiungere i file di progetto.

---
+++ ScreenshotBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using ScreenshotBot.Services;
+using System;
+using System.IO;
+using System.Timers;
+
+string outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots");
+Directory.CreateDirectory(outputFolder);
+
+int intervalSeconds = 10;
+
+Console.WriteLine("🟢 ScreenshotBot avviato.");
+Console.WriteLine($"Ogni {intervalSeconds} secondi verrà catturato uno screenshot.");
+Console.WriteLine("Premi INVIO per fermare il bot.\n");
+
+System.Timers.Timer timer = new(intervalSeconds * 1000);
+timer.Elapsed += (sender, e) => ScreenshotService.CaptureScreenshot(outputFolder);
+timer.Start();
+
+Console.ReadLine();
+
+Console.WriteLine("🛑 Bot fermato. Uscita...");
+timer.Stop();
+timer.Dispose();
---
+++ ScreenshotBot/Services/ScreenshotService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ScreenshotBot.Services
+{
+    public static class ScreenshotService
+    {
+        public static void CaptureScreenshot(string outputDir)
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+
+            using Bitmap bitmap = new(bounds.Width, bounds.Height);
+            using Graphics g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+
+            string filename = $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+            string path = Path.Combine(outputDir, filename);
+
+            bitmap.Save(path, ImageFormat.Png);
+
+            Console.WriteLine($"[✔] Screenshot salvato: {filename}");
+        }
+    }
+}
